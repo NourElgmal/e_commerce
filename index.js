04d22b3552f1dec5844b34e9d95f63e1130cc077
NourElgmal <nouralgmal123@gmail.com>
2024-08-22 19:31:01 +0300

    e_commerce app

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const port = process.env.PORT || 3000;
 conct();
 app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/test", (req, res) => res.send("tesst secc"));
+app.use("/category", require("../src/category/api.category"));
 app_use(app);
 app.all("*", (req, res, next) => {
   next(new Apperr("not found the url", 404));
